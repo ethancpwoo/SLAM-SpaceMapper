5fe2321c5aa24a4220bd2e3008f6fa3338ae8f6d
ethancpwoo <ethan.cpw.woo@outlook.com>
2024-06-28 14:14:10 -0400

    reinstalling g2o

--- include/backend.h
+++ include/backend.h
@@ -4,51 +4,64 @@
 
 #include "common.h"
 
-#include <g2o/core/base_binary_edge.h>
-#include <g2o/core/base_unary_edge.h>
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/block_solver.h>
-#include <g2o/core/optimization_algorithm_gauss_newton.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
-#include <g2o/core/robust_kernel.h>
-#include <g2o/core/solver.h>
-#include <g2o/core/sparse_optimizer.h>
-#include <g2o/solvers/csparse/linear_solver_csparse.h>
-#include <g2o/solvers/dense/linear_solver_dense.h>
+// #include <g2o/core/robust_kernel.h>
+// #include <g2o/core/sparse_optimizer.h>
+// #include <g2o/solvers/eigen/linear_solver_eigen.h>
 
 
 namespace slam {
 
-class VertexSE3 : public g2o::BaseVertex<6, Sophus::SE3> {
+// class VertexSE3 : public g2o::BaseVertex<6, Sophus::SE3d> {
 
-    public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-        virtual void setToOriginImpl() override;
-        virtual void oplusImpl(const double *update) override;
-        virtual bool read(std::istream &in) override { return true; }
-        virtual bool write(std::ostream &out) const override { return true; }
+//     public:
+//         EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+        
+//         virtual void setToOriginImpl() override {
+//             _estimate = Sophus::SE3d();
+//         }
 
-};
+//         virtual void oplusImpl(const double *update) override {
+//             Eigen::Matrix<double, 6, 1> update_eigen;
+//             update_eigen << update[0], update[1], update[2], update[3], update[4], update[5];
+//             _estimate = Sophus::SE3d::exp(update_eigen) * _estimate;        
+//         }
 
-class VertexFeaturePos : public g2o::BaseVertex<3, Eigen::Matrix<double, 3, 1>> {
+//         virtual bool read(std::istream &in) override { return true; }
+//         virtual bool write(std::ostream &out) const override { return true; }
+
+// };
+
+// class VertexFeaturePos : public g2o::BaseVertex<3, Eigen::Matrix<double, 3, 1>> {
     
-    public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
-        virtual void setToOriginImpl() override; 
-        virtual void oplusImpl(const double *update) override;
-        virtual bool read(std::istream &in) override {return true};
-        virtual bool write(std::ostream &out) override {return true};
+//     public:
+//         EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+        
+//         virtual void setToOriginImpl() override {
+//             _estimate = Eigen::Matrix<double, 3, 1>::Zero();
+//         }
 
-};
+//         virtual void oplusImpl(const double *update) override {
+//             _estimate[0] = update[0];
+//             _estimate[1] = update[1];
+//             _estimate[2] = update[2];
+//         }
+
+//         virtual bool read(std::istream &in) override {return true;}
+//         virtual bool write(std::ostream &out) const override {return true;}
+
+// };
 
 class Backend {
 
     public:
         Backend();
-        void BundleAdjustment();
-
-    private:
-        
+        void BundleAdjustment(
+            std::vector<Sophus::SE3d> &poses, 
+            std::vector<std::vector<Eigen::Matrix<double, 3, 1>>> &positions
+        );
 
 };
     
--- src/backend.cpp
+++ src/backend.cpp
@@ -1,4 +1,4 @@
-#include "slam/backend.h"
+#include "backend.h"
 
 namespace slam {
 
@@ -6,64 +6,48 @@ Backend::Backend() {
     
 }
 
-Backend::BundleAdjustment(
-    const std::vector<Sophus::SE3d>> &poses, 
-    const std::vector<std::vector<Eigen::Matrix<double, 3, 1>>> &positions) {
-    typedef g2o::LinearSolverCSparce<g2o::BlockSolver_6_3::PoseMatrixType> LinearSolverType;
+void Backend::BundleAdjustment(
+    std::vector<Sophus::SE3d> &poses, 
+    std::vector<std::vector<Eigen::Matrix<double, 3, 1>>> &positions) {
 
-    auto solver = new g2o::OptimizationAlgorithmLevenberg(
-        g2o::make_unique<g2o::BlockSolver_6_3>(
-            g2o::make_unique<LinearSolverType>()
-        )
-    );
+    // std::unique_ptr<g2o::BlockSolverX::LinearSolverType> linearSolver(
+    //     new g2o::LinearSolverEigen<g2o::BlockSolverX::PoseMatrixType>()
+    // );
+    // std::unique_ptr<g2o::BlockSolverX> solver_ptr (new g2o::BlockSolverX(std::move(linearSolver)));
+    // g2o::OptimizationAlgorithmLevenberg *solver = new g2o::OptimizationAlgorithmLevenberg(std::move(solver_ptr));
 
-    g2o::SparseOptimizer optimizer;
-    optimizer.setAlgorithm(solver);
+    // g2o::SparseOptimizer optimizer;
+    // optimizer.setAlgorithm(solver);
+    // optimizer.setVerbose(true);
 
-    std::unordered_map<unsigned long, VertexSE3*> vertices;
+    // std::unordered_map<unsigned long, VertexSE3*> vertices;
 
-    for (int i = 0; i < poses.size(); i++) {
-        VertexPose *vertex_pose = new VertexPose();
-        vertex_pose->setId(i);
-        vertex_pose->setEstimate(poses[i]);
-        optimizer.addVertex(vertex_pose);
-        verticies.insert({i, vertex_pose});
-    }
+    // for (int i = 0; i < poses.size(); i++) {
+    //     VertexSE3 *vertex_pose = new VertexSE3();
+    //     vertex_pose->setId(i);
+    //     vertex_pose->setEstimate(poses[i]);
+    //     optimizer.addVertex(vertex_pose);
+    //     vertices.insert({i, vertex_pose});
+    // }
 
-    int index = 1;
-    double chi = 5.991;
-    std::unordered_map<unsigned long, VertexFeaturePos*> vertices_features;
+    // int index = 1;
+    // double chi = 5.991;
+    // std::unordered_map<unsigned long, VertexFeaturePos*> vertices_features;
     
-    for (int i = 0; i < positions.size(); i++) {
-        for(int j = 0; j < positions[i].size(); j++) {
-            VertexFeaturePos *vertex_feature = new VertexFeaturePos;
-            vertex_feature->setEstimate(positions[i][j]);
-            vertex_feature->setId(poses.size() + j);
-            vertex_feature->setMarginalized(true);
-            vertices_features.insert({poses.size() + j, vertex_feature});
-        }
-    }
+    // for (int i = 0; i < positions.size(); i++) {
+    //     for(int j = 0; j < positions[i].size(); j++) {
+    //         VertexFeaturePos *vertex_feature = new VertexFeaturePos;
+    //         vertex_feature->setEstimate(positions[i][j]);
+    //         vertex_feature->setId(poses.size() + j);
+    //         vertex_feature->setMarginalized(true);
+    //         vertices_features.insert({poses.size() + j, vertex_feature});
+    //     }
+    // }
+    
+    // delete solver;
+    // solver_ptr.release();
+    // linearSolver.release();
 
-}
-
-virtual void VertexSE3::setToOriginImpl() override {
-    _estimate = Sophus::SE3();
-}
-
-virtual void VertexSE3::oplusImpl(const double *update) override {
-    Eigen::Matrix<double, 6, 1> update_eigen;
-    update_eigen << update[0], update[1], update[2], update[3], update[4], update[5];
-    _estimate = Sophus::SE3::exp(update_eigen) * _estimate;        
-}
-
-virtual void VertexFeaturePos::setToOriginImpl() override {
-    _estimate = Eigen::Matrix<double, 3, 1>;
-}
-
-virtual void VertexFeaturePos::oplusImpl(const double *update) override {
-    _estimate[0] = update[0];
-    _estimate[1] = update[1];
-    _estimate[2] = update[2];
 }
 
 }
